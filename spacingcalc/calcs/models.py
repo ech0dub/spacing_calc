72a6bcf0bde97a2c00c0c7d31d6fe89bbef3c484
David Spackman <dmspackman@gmail.com>
2017-08-11 16:34:01 +1200

    start django interface 3

--- spacingcalc/calcs/models.py
+++ spacingcalc/calcs/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+
+class Layout(models.Model):
+    depth = models.FloatField(default=1.5)
+    arrangement = models.CharField(max_length=200)
+    cable_spacing_m = models.FloatField(default=0)
+    outer_diameter_m = models.FloatField(default=0.1)
--- spacingcalc/spacingcalc/settings.py
+++ spacingcalc/spacingcalc/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'calcs.apps.CalcsConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
