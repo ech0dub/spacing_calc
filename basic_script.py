ba0d30e82f5dd0187eb9956e581e82bc67d23977
David Spackman <dmspackman@gmail.com>
2017-08-11 14:53:40 +1200

    updated script to class structure

--- basic_script.py
+++ basic_script.py
@@ -1,30 +1,71 @@
 import math
 
-# Inputs
-outer_diameter_mm = 100  # mm
-outer_diameter_m = outer_diameter_mm / 1000
-depth_to_top_m = 1.5
-arrangement_options = ['touching trefoil', 'spaced trefoil']
-arrangement_selection = arrangement_options[0]
-if arrangement_selection == 'spaced trefoil':
-    cable_spacing_mm = 300
-    cable_spacing_m = cable_spacing_mm / 1000
-else:
-    cable_spacing_m = outer_diameter_m
 
-# Calculate coordinates
-cable_1_x = 0
-cable_1_y = depth_to_top_m + (outer_diameter_m / 2)
-cable_2_x = cable_1_x - (cable_spacing_m / 2)
-cable_2_y = cable_1_y + (math.sqrt(3) / 2) * cable_spacing_m
-cable_3_x = cable_1_x + (cable_spacing_m / 2)
-cable_3_y = cable_1_y + (math.sqrt(3) / 2) * cable_spacing_m
+class Cable:
+    def __init__(self, label):
+        self.label = label
+        self.x_m = 0
+        self.y_m = 0
 
-# Print output
-for output in [[cable_1_x, cable_1_y],
-               [cable_2_x, cable_2_y],
-               [cable_3_x, cable_3_y]]:
-    print('x: {:6.3f}m  y: {:6.3f}m'.format(output[0], output[1]))
 
-# Display graphic
-# still to write
+class Circuit:
+
+    def __init__(self, depth_to_top_m, arrangement, cable_spacing_m, outer_diameter_m):
+        self.depth_to_top_m = depth_to_top_m
+        self.arrangement = arrangement
+        self.cable_spacing_m = cable_spacing_m
+        self.outer_diameter_m = outer_diameter_m
+        self.cable1 = Cable('Yellow phase')
+        self.cable2 = Cable('Red phase')
+        self.cable3 = Cable('Blue phase')
+
+    def calc_spacing(self):
+        if self.arrangement == 'spaced trefoil':
+            pass
+        elif self.arrangement == 'touching trefoil':
+            self.cable_spacing_m = self.outer_diameter_m
+        else:
+            raise ValueError('Error: Arrangement specified as {}./n'
+                             'This arrangement is not supported.'.format(str(self.arrangement)))
+
+    def calc_coords(self):
+        self.calc_spacing()
+        self.cable1.x_m = 0
+        self.cable1.y_m = self.depth_to_top_m + (self.outer_diameter_m / 2)
+        self.cable2.x_m = self.cable1.x_m - (self.cable_spacing_m / 2)
+        self.cable2.y_m = self.cable1.y_m + (math.sqrt(3) / 2) * self.cable_spacing_m
+        self.cable3.x_m = self.cable1.x_m + (self.cable_spacing_m / 2)
+        self.cable3.y_m = self.cable1.y_m + (math.sqrt(3) / 2) * self.cable_spacing_m
+
+
+def main():
+    def print_output(cable_circuit):
+        for cable in (cable_circuit.cable1, cable_circuit.cable2, cable_circuit.cable3):
+            print('{:12s}: x: {:6.3f}m  y: {:6.3f}m'.format(cable.label, cable.x_m, cable.y_m))
+
+    def display_graphic_output():
+        pass
+
+    # Input parameter options
+    arrangement_options = ['touching trefoil', 'spaced trefoil']
+
+    # Init circuit
+    circuit1 = Circuit(depth_to_top_m=1.5,
+                       arrangement=arrangement_options[0],
+                       cable_spacing_m=0,
+                       outer_diameter_m=0.1)
+
+    # Get new inputs from user and update circuit
+    circuit1.depth_to_top_m = float(input("Enter depth to top of upper cable [m]: "))
+    arrangement_choice = int(input("Enter 0 for touching trefoil, 1 for spaced trefoil: "))
+    circuit1.arrangement = arrangement_options[arrangement_choice]
+    if circuit1.arrangement == "spaced trefoil":
+        circuit1.cable_spacing_m = int(input("Enter cable spacing [mm]: ")) / 1000
+    circuit1.outer_diameter_m = int(input("Enter cable outer diameter [mm]: ")) / 1000
+
+    circuit1.calc_coords()
+
+    print_output(circuit1)
+    display_graphic_output()
+
+main()
