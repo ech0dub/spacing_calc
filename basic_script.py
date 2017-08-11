12f878c9a37e5e7f087113df11541ea23c884545
David Spackman <dmspackman@gmail.com>
2017-08-11 12:54:12 +1200

    update comments

--- basic_script.py
+++ basic_script.py
@@ -12,7 +12,7 @@ if arrangement_selection == 'spaced trefoil':
 else:
     cable_spacing_m = outer_diameter_m
 
-# Outputs
+# Calculate coordinates
 cable_1_x = 0
 cable_1_y = depth_to_top_m + (outer_diameter_m / 2)
 cable_2_x = cable_1_x - (cable_spacing_m / 2)
@@ -20,8 +20,11 @@ cable_2_y = cable_1_y + (math.sqrt(3) / 2) * cable_spacing_m
 cable_3_x = cable_1_x + (cable_spacing_m / 2)
 cable_3_y = cable_1_y + (math.sqrt(3) / 2) * cable_spacing_m
 
+# Print output
 for output in [[cable_1_x, cable_1_y],
                [cable_2_x, cable_2_y],
                [cable_3_x, cable_3_y]]:
     print('x: {:6.3f}m  y: {:6.3f}m'.format(output[0], output[1]))
 
+# Display graphic
+# still to write
